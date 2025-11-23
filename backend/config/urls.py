918df8309a72c1fbe76c74b2a9db113adefababb
Shashrwat Shukla <shashrwatshukla1325@gmail.com>
2025-11-23 21:04:59 +0530

    Initials

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
-# We removed 're_path' and 'TemplateView' imports because we don't need them anymore.
+@api_view(['GET', 'HEAD'])
+@permission_classes([AllowAny])
+def health_check(request):
+    """Health check endpoint"""
+    return Response({'status': 'ok', 'message': 'Chemical Equipment Visualizer API is running'})
 
 urlpatterns = [
+    path('', health_check, name='health-check'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
